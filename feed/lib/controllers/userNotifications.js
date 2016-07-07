3d35a0e654de07e958cf6f844487c397518b494e
camilin87 <camilin87@gmail.com>
2016-07-06 20:34:54 -0400

    no need to keep this here

--- feed/lib/controllers/userNotifications.js
+++ feed/lib/controllers/userNotifications.js
@@ -2,19 +2,6 @@ module.exports = function (apiKey, webError) {
     return {
         recent: function(userId, maxCount, reqApiKey, response){
             webError.unauthorized(response, "Unauthorized");
-
-            console.log(
-                "recent notifications",
-                "twitter_user_id", userId,
-                "max_result_count", maxCount,
-                "reqApiKey", reqApiKey
-            );
-
-            response.send([
-                {id: 1, type: "unfollow"},
-                {id: 2, type: "follow"},
-                {id: 3, type: "unfollow"}
-            ]);
         }
     }
 }
