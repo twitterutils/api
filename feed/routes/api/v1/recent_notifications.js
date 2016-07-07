aa3fb036676a2f7df67fff01ed827841e4a73c5b
camilin87 <camilin87@gmail.com>
2016-07-06 20:24:25 -0400

    read the api key

--- feed/routes/api/v1/recent_notifications.js
+++ feed/routes/api/v1/recent_notifications.js
@@ -6,7 +6,7 @@ router.get("/:twitter_user_id/:max_result_count?", (req, res, next) => {
     userNotifications().recent(
         req.params.twitter_user_id,
         req.params.max_result_count || 100,
-        "the api key",
+        req.headers.authorization,
         res
     );
 });
