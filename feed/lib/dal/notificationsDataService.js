5cd15bb6ebd2c90345e28d796698d91eef654194
camilin87 <camilin87@gmail.com>
2016-07-07 08:18:20 -0400

    the notifications max count needs to be an int

--- feed/lib/dal/notificationsDataService.js
+++ feed/lib/dal/notificationsDataService.js
@@ -9,7 +9,7 @@ module.exports = function (db) {
                 db
                     .collection("notifications_details")
                     .find({userId: userId})
-                    .limit(maxCount)
+                    .limit(parseInt(maxCount))
                     .toArray()
                     .then(fulfill, reject);
             });
