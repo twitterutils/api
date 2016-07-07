a8d307cf886a63e0683659c66184eb97a87e6c94
camilin87 <camilin87@gmail.com>
2016-07-07 07:47:18 -0400

    call the controller with its depedendencies

--- feed/routes/api/v1/recent_notifications.js
+++ feed/routes/api/v1/recent_notifications.js
@@ -1,9 +1,18 @@
 var rfr = require("rfr");
 var router = require("express").Router();
+var dbConnectionFactory = rfr("lib/webApiMongoDbConnectionFactory");
+var apiKey = rfr("lib/simpleApiKeyValidation");
+var webError = rfr("lib/webApiError");
 var userNotifications = rfr("feed/lib/controllers/userNotifications");
+var notificationsDataService = rfr("feed/lib/dal/notificationsDataService");
 
 router.get("/:twitter_user_id/:max_result_count?", (req, res, next) => {
-    userNotifications().recent(
+    userNotifications(
+        dbConnectionFactory,
+        notificationsDataService,
+        apiKey,
+        webError
+    ).recent(
         req.params.twitter_user_id,
         req.params.max_result_count || 100,
         req.headers.authorization,
