e25c59874bf300335279ee63acdf9bc54ae8c50c
camilin87 <camilin87@gmail.com>
2016-07-23 11:03:50 -0400

    remove redundant promise

--- usernames/lib/dal/usernamesDataService.js
+++ usernames/lib/dal/usernamesDataService.js
@@ -1,21 +1,16 @@
-var promise = require("the-promise-factory");
-
 module.exports = function (db) {
     if (!db) throw new Error("A database is required");
 
     return {
         find: function(userIds){
-            return promise.create((fulfill, reject) => {
-                userIds = (userIds || []).map((id) => {
-                    return id.toString();
-                });
-
-                db
-                    .collection("usernames_list")
-                    .find({id: {$in: userIds}})
-                    .toArray()
-                    .then(fulfill, reject);
+            userIds = (userIds || []).map((id) => {
+                return id.toString();
             });
+
+            return db
+                .collection("usernames_list")
+                .find({id: {$in: userIds}})
+                .toArray();
         }
     }
 }
