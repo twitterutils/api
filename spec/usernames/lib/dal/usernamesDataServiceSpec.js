f896a9cd4813deadbf4d19f6dc41c44eac925f50
camilin87 <camilin87@gmail.com>
2016-07-23 10:52:40 -0400

    some test infrastructure

--- spec/usernames/lib/dal/usernamesDataServiceSpec.js
+++ spec/usernames/lib/dal/usernamesDataServiceSpec.js
@@ -7,4 +7,44 @@ describe("usernamesDataService", function(){
             usernamesDataService();
         }).toThrow(new Error("A database is required"));
     });
+
+    var dbRequests = null;
+    var db = null;
+    var seededError = null;
+    var seededResult = null;
+
+    beforeEach(function(){
+        seededError = null;
+        seededResult = null;
+        dbRequests = [];
+
+        db = {
+            collection: function(tableName){
+                if (tableName === "usernames_list"){
+                    return {
+                        find: function(req){
+                            dbRequests.push(req);
+                            return {
+                                toArray: function(){
+                                     return promise.create((fulfill, reject) => {
+                                        if (seededError){
+                                            reject(seededError);
+                                            return;
+                                        }
+                                        
+                                        if (seededResult){
+                                            fulfill(seededResult);
+                                            return;
+                                        }
+
+                                        return jasmine.getEnv().fail("Invalid seeding");
+                                    });
+                                }
+                            }
+                        }
+                    };
+                }
+            }
+        };
+    });
 });
