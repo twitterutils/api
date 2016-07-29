a1390e5cd4b63fbd206266e4670c9fed7f32c65c
camilin87 <camilin87@gmail.com>
2016-07-29 09:03:54 -0400

    use the controller in the route

--- public/routes/api/v1/user_feed.js
+++ public/routes/api/v1/user_feed.js
@@ -2,12 +2,20 @@ var rfr = require("rfr");
 var router = require("express").Router();
 var cors = require('cors');
 var cache = rfr("lib/theApiCache");
+var dbConnectionFactory = rfr("lib/webApiMongoDbConnectionFactory");
+var webError = rfr("lib/webApiError");
+var userFeed = rfr("public/lib/controllers/userFeed");
+var userFeedDataService = rfr("public/lib/dal/userFeedDataService");
 
 router.get("/:user_name", cors(), cache.longLived(), (req, res, next) => {
-    res.send({items: [
-        "item1",
-        "item2"
-    ]});
+    userFeed(
+        dbConnectionFactory,
+        userFeedDataService,
+        webError
+    ).read(
+        req.params.user_name,
+        res
+    );
 });
 
 module.exports = router;
