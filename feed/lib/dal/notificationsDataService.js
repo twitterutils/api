9628187b4cd5352a64420ffce2f6e2e014acf348
camilin87 <camilin87@gmail.com>
2016-07-29 08:37:14 -0400

    remove redundant promise

--- feed/lib/dal/notificationsDataService.js
+++ feed/lib/dal/notificationsDataService.js
@@ -1,18 +1,13 @@
-var promise = require("the-promise-factory");
-
 module.exports = function (db) {
     if (!db) throw new Error("A database is required");
 
     return {
         recentNotifications: function(userId, maxCount){
-            return promise.create((fulfill, reject) => {
-                db
-                    .collection("notifications_details")
-                    .find({userId: userId.toString()})
-                    .limit(parseInt(maxCount))
-                    .toArray()
-                    .then(fulfill, reject);
-            });
+            return db
+                .collection("notifications_details")
+                .find({userId: userId.toString()})
+                .limit(parseInt(maxCount))
+                .toArray();
         }
     };
 }
