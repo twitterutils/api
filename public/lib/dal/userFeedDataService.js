41aa7d2f694d9341123c08ab8087ce897c649267
camilin87 <camilin87@gmail.com>
2016-07-29 08:54:45 -0400

    fails on db error

--- public/lib/dal/userFeedDataService.js
+++ public/lib/dal/userFeedDataService.js
@@ -1,3 +1,17 @@
+var promise = require("the-promise-factory");
+
 module.exports = function (db) {
     if (!db) throw new Error("A database is required");
+
+    return {
+        read: (userName) => {
+            return promise.create((fulfill, reject) => {
+                db
+                    .collection("feed_list")
+                    .findOne({}, (err, result) => {
+                        reject(err);
+                    });
+            });
+        }
+    }
 }
--- spec/public/lib/dal/userFeedDataServiceSpec.js
+++ spec/public/lib/dal/userFeedDataServiceSpec.js
@@ -16,36 +16,36 @@ describe("userFeedDataService", function(){
     var dataService = null;
 
     beforeEach(function(){
-        var db = {
-            collection: function(collectionName){
-                if (collectionName === "feed_list") {
-                    return collection;
-                }
-            }
-        };
-
         dbRequests = [];
         findSeededResult = {};
         findSeededError = null;
-        var collection = {
-            findOne: function(dbRequest){
-                dbRequests.push(dbRequests);
+
+        var db = {
+            collection: function(collectionName){
+                if (collectionName !== "feed_list") {
+                    return jasmine.getEnv().fail("invalid invocation");
+                }
 
                 return {
-                    then: (fulfill, reject) => {
-                        if (findSeededError){
-                            reject(findSeededError);
-                            return;
-                        }
-
-                        fulfill(findSeededResult)
+                    findOne: function(dbRequest, callback){
+                        dbRequests.push(dbRequest);
+                        callback(findSeededError, findSeededResult);
                     }
-                };
+                }
             }
         };
 
         dataService = userFeedDataService(db);
     });
 
-    
+    it ("fails on db failure", function(done){
+        findSeededError = "db error";
+
+        dataService
+            .read("333444")
+            .then(null, (err) => {
+                expect(err).toBe(findSeededError);
+                done();
+            });
+    })
 })
