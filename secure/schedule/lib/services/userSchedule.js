6ecf53923f13003e846a8b12c8c1e230f4a3094f
Camilo Sanchez <camilin87@gmail.com>
2016-08-24 15:38:11 -0400

    refactor the recipe for async

--- secure/schedule/lib/services/userSchedule.js
+++ secure/schedule/lib/services/userSchedule.js
@@ -1,32 +1,58 @@
 var promise = require("the-promise-factory");
+var async = require("async");
 
 module.exports = function (userScheduleDataService) {
     return {
         update: (userId) => {
             return promise.create((fulfill, reject) => {
-
-                userScheduleDataService
-                    .first(userId)
-                    .then((scheduleInfo) => {
-                        if (!scheduleInfo){
-                            scheduleInfo = { readCount: 0 };
-                        }
-
-                        var updatedStatus = {
-                            readCount: scheduleInfo.readCount + 1
-                        };
-
-                        console.log("Updating-User-Schedule",
-                            "userId=", userId,
-                            "readCount", updatedStatus.readCount
-                        );
-
-                        userScheduleDataService
-                            .update(userId, updatedStatus)
-                            .then(fulfill, reject);
-
-                    }, reject);
+                async.waterfall([
+                    readScheduleInfo(userId),
+                    updateScheduleInfo(userId),
+                    saveScheduleInfo(userId)
+                ], (err) => {
+                    if (err) return reject(err);
+                    fulfill();
+                });
             })
+        }
+    }
+
+    function readScheduleInfo(userId) {
+        return (callback) => {
+            userScheduleDataService
+                .first(userId)
+                .then((result) => {
+                    callback(null, result);
+                }, callback);
+        }
+    }
+
+    function updateScheduleInfo(userId){
+        return (scheduleInfo, callback) => {
+            if (!scheduleInfo){
+                scheduleInfo = { readCount: 0 };
+            }
+
+            var updatedStatus = {
+                readCount: scheduleInfo.readCount + 1
+            };
+
+            console.log("Updating-User-Schedule",
+                "userId=", userId,
+                "readCount", updatedStatus.readCount
+            );
+
+            callback(null, updatedStatus);
+        }
+    }
+
+    function saveScheduleInfo(userId){
+        return (scheduleInfo, callback) => {
+            userScheduleDataService
+                .update(userId, scheduleInfo)
+                .then(() => {
+                    callback(null)
+                }, callback);
         }
     }
 }
