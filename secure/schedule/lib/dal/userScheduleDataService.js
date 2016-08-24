45f156807a3fff2bd869889b414040d13dab5d09
camilin87 <camilin87@gmail.com>
2016-08-24 08:54:26 -0400

    start building the test infrastructure

--- secure/schedule/lib/dal/userScheduleDataService.js
+++ secure/schedule/lib/dal/userScheduleDataService.js
@@ -1,3 +1,7 @@
-module.exports = function() {
-    throw new Error("A database is required");
+var promise = require("the-promise-factory");
+var isoDateStr = require("iso-date-str");
+
+module.exports = function(db, dateSvc) {
+    if (!db) throw new Error("A database is required");
+    if (!dateSvc) dateSvc = isoDateStr;
 }
--- spec/secure/schedule/dal/userScheduleDataServiceSpec.js
+++ spec/secure/schedule/dal/userScheduleDataServiceSpec.js
@@ -8,4 +8,30 @@ describe("userScheduleDataService", function() {
             userScheduleDataService();
         }).toThrow(new Error("A database is required"));
     });
+
+    var dataService = null;
+    var collectionStub = null;
+
+    beforeEach(function(){
+        collectionStub = {
+            updateOne: function(){},
+            findOne: function(){}
+        };
+
+        var db = {
+            collection: function(collectionName){
+                if (collectionName === "schedule_user_status"){
+                    return collectionStub;
+                }
+            }
+        };
+
+        var invocation = 0;
+        var currentDateSvc = () => {
+            invocation++;
+            return `now${invocation}`
+        };
+
+        dataService = userScheduleDataService(db, currentDateSvc);
+    })
 })
