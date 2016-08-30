f252d46111787dbe597c6aeba25a5a72ccf966c6
camilin87 <camilin87@gmail.com>
2016-08-29 20:51:49 -0400

    start defining the schedule list data service

---
+++ spec/secure/schedule/dal/scheduleListDataServiceSpec.js
@@ -0,0 +1,11 @@
+var rfr = require("rfr");
+var promise = require("the-promise-factory");
+var scheduleListDataService = rfr("secure/schedule/lib/dal/scheduleListDataService");
+
+describe("scheduleListDataService", function () {
+    it("requires a database", function(){
+        expect(function(){
+            scheduleListDataService();
+        }).toThrow(new Error("A database is required"));
+    });
+})
