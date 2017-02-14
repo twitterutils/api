2de872c2661612ac09785c14d34b13ee9c2aa5b5
Camilo Sanchez <camilin87@gmail.com>
2017-02-13 19:58:10 -0500

    remove leading and trailing quotes from the variable values returned from heroku

--- cronjobs/rakefile.rb
+++ cronjobs/rakefile.rb
@@ -14,13 +14,25 @@ end
 
 task :env => [:cleanup] do
     all_env_variables_raw = run_s "heroku config --app twu-api --shell | grep -v TWU_API"
+    all_env_variables_list = all_env_variables_raw.split
 
-    var_names = all_env_variables_raw.split.map { |kvstr| kvstr.split("=")[0] }.join(",")
+    env_variables = all_env_variables_list
+        .map { |kvstr| kvstr.split("=") }
+        .map { |kvp| [kvp[0], kvp[1].chomp("'").reverse.chomp("'").reverse] }
+        .map { |kvp| "#{kvp[0]}=#{kvp[1]}" }
+    # puts env_variables
+
+    all_var_names = all_env_variables_list
+        .map { |kvstr| kvstr.split("=")[0] }
+        .join(",")
 
     File.open(ENV_FILE_NAME, 'w') do |f|
         f.puts "TASK_SCHEDULE=* * * * 10"
-        f.puts "COPY_ENV_VARS=#{var_names}"
-        f.puts all_env_variables_raw
+        f.puts "COPY_ENV_VARS=#{all_var_names}"
+
+        env_variables.each { |l|
+            f.puts l
+        }
     end
 
     # puts `cat #{ENV_FILE_NAME}`
