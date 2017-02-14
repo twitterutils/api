53c42417f47bc383038647f25b0891a57cb84954
Camilo Sanchez <camilin87@gmail.com>
2017-02-13 22:03:58 -0500

    start building the foundation to generate a huge env command

--- cronjobs/rakefile.rb
+++ cronjobs/rakefile.rb
@@ -12,9 +12,23 @@ def run(command, silent = false)
     command_output
 end
 
-task :env => [:cleanup] do
+def read_env_variables_list
     all_env_variables_raw = run_s "heroku config --app twu-api --shell | grep -v TWU_API"
-    all_env_variables_list = all_env_variables_raw.split
+    all_env_variables_raw.split
+end
+
+def generate_copy_vars(vars_list)
+    all_var_names = vars_list
+        .map { |kvstr| kvstr.split("=")[0] }
+        .join(",")
+    "COPY_ENV_VARS=#{all_var_names}"
+end
+
+task :env => [:cleanup, :env_file, :env_cmd] do
+end
+
+task :env_file do
+    all_env_variables_list = read_env_variables_list
 
     env_variables = all_env_variables_list
         .map { |kvstr| kvstr.split("=") }
@@ -22,13 +36,9 @@ task :env => [:cleanup] do
         .map { |kvp| "#{kvp[0]}=#{kvp[1]}" }
     # puts env_variables
 
-    all_var_names = all_env_variables_list
-        .map { |kvstr| kvstr.split("=")[0] }
-        .join(",")
-
     File.open(ENV_FILE_NAME, 'w') do |f|
         f.puts "TASK_SCHEDULE=*/10 * * * *"
-        f.puts "COPY_ENV_VARS=#{all_var_names}"
+        f.puts generate_copy_vars(env_variables)
 
         env_variables.each { |l|
             f.puts l
@@ -36,6 +46,10 @@ task :env => [:cleanup] do
     end
 
     # puts `cat #{ENV_FILE_NAME}`
+end
+
+task :env_cmd do
+    # all_env_variables_list = read_env_variables_list
 end
 
 task :cleanup do
