eaea8254dea0c9cdd709c2a89378c6a215cbf7fb
Camilo Sanchez <camilin87@gmail.com>
2017-03-02 09:51:47 -0500

    remove the semicolons

--- cronjobs/autounfollow/lib/services/twitterFollowersService.js
+++ cronjobs/autounfollow/lib/services/twitterFollowersService.js
@@ -1,22 +1,22 @@
-var promise = require("the-promise-factory");
-var twitterNodeClientFactory = require("twitter-node-client-factory");
-var throttleFactory = require("throttle-factory");
+var promise = require("the-promise-factory")
+var twitterNodeClientFactory = require("twitter-node-client-factory")
+var throttleFactory = require("throttle-factory")
 
 module.exports = function (registeredUsersDataService, twitterClientFactory, twitterThrottleFactory) {
     if (!twitterClientFactory){
-        twitterClientFactory = twitterNodeClientFactory;
+        twitterClientFactory = twitterNodeClientFactory
     }
 
     if (!twitterThrottleFactory){
-        twitterThrottleFactory = throttleFactory;
+        twitterThrottleFactory = throttleFactory
     }
 
-    var throttle = twitterThrottleFactory(15, 300000);
+    var throttle = twitterThrottleFactory(15, 300000)
 
     return {
         unfollow: function(credentials, userId){
             return promise.create((fulfill, reject) => {
-                console.log("twitterFollowersService.unfollow; originator=", credentials.id, "target=", userId);
+                console.log("twitterFollowersService.unfollow originator=", credentials.id, "target=", userId)
                 throttle.execute(() => {
                     var twitterClient = twitterClientFactory(
                         credentials.oauth_access_token,
@@ -24,33 +24,33 @@ module.exports = function (registeredUsersDataService, twitterClientFactory, twi
                         null,
                         "TWU_CRON_AUTOUNFOLLOW_TWITTER_CONSUMER_KEY",
                         "TWU_CRON_AUTOUNFOLLOW_TWITTER_CONSUMER_SECRET"
-                    );
+                    )
                     twitterClient.doPost(
                         twitterClient.baseUrl + "/friendships/destroy.json",
                         {user_id: userId},
                         (err) => {
                             if (shouldDisableUser(err)){
-                                disableUser(credentials.id).then(fulfill, reject);
-                                return;
+                                disableUser(credentials.id).then(fulfill, reject)
+                                return
                             }
 
                             if (shouldIgnoreFriendshipDoesNotExistError(err)){
-                                fulfill();
-                                return;
+                                fulfill()
+                                return
                             }
 
-                            reject(err);
+                            reject(err)
                         },
                         (apiResponseInfoStr) => {
-                            var apiResponse = JSON.parse(apiResponseInfoStr);
+                            var apiResponse = JSON.parse(apiResponseInfoStr)
 
-                            fulfill(apiResponse.id);
+                            fulfill(apiResponse.id)
                         }
-                    );
-                });
-            });
+                    )
+                })
+            })
         }
-    };
+    }
 
     function shouldDisableUser(error){
         return errorContains(error, 403, 220, "UnauthorizedError")
@@ -62,27 +62,27 @@ module.exports = function (registeredUsersDataService, twitterClientFactory, twi
 
     function errorContains(error, responseCode, code, message){
          if (error.statusCode !== responseCode){
-            return false;
+            return false
         }
 
-        var errorData = JSON.parse(error.data);
-        var result = false;
+        var errorData = JSON.parse(error.data)
+        var result = false
 
         errorData.errors.forEach((element, index, array) => {
             if (element.code === code){
-                result = true;
+                result = true
             }
-        });
+        })
 
         if (result){
-            console.log(message, error);
+            console.log(message, error)
         }
 
-        return result;
+        return result
     }
 
     function disableUser(userId){
-        console.log("Disable", "user_id=", userId);
+        console.log("Disable", "user_id=", userId)
 
         return registeredUsersDataService.disable(userId)
     }
