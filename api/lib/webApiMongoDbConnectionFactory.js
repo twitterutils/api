d647234b147760656e23f82a8cf9065addfafa9f
Camilo Sanchez <camilin87@gmail.com>
2017-04-22 14:53:28 -0400

    try to read a default connection string variable if none is provided

--- api/lib/webApiMongoDbConnectionFactory.js
+++ api/lib/webApiMongoDbConnectionFactory.js
@@ -4,12 +4,23 @@ var webError = rfr("lib/webApiError");
 var MongoClient = require("mongodb").MongoClient;
 
 module.exports = function(res, envSettingName){
+    var DEFAULT_CONNECTION_STRING_NAME = "CONNECTION_STRING";
+
     if (!envSettingName){
-        envSettingName = "CONNECTION_STRING";
+        envSettingName = DEFAULT_CONNECTION_STRING_NAME;
     }
 
     return promise.create((fulfill, reject) => {
         var dbConnection = process.env[envSettingName];
+
+        if ((dbConnection || "").length == 0){
+            dbConnection = process.env[DEFAULT_CONNECTION_STRING_NAME];
+        }
+
+        if ((dbConnection || "").length == 0){
+            console.error("Empty connection string");
+        }
+
         MongoClient.connect(dbConnection, function(err, db){
             if (err){
                 webError.unexpected(res, "Db Connection Failure", err);
