525f6fd14c3e2757f7071e99b71c067c53dcb35d
Camilo Sanchez <camilin87@gmail.com>
2017-04-22 14:50:20 -0400

    build infrastructure to get all the environment variables

--- cronjobs/rakefile.rb
+++ cronjobs/rakefile.rb
@@ -1,6 +1,7 @@
 task :default => [:e]
 
 ENV_FILE_NAME = ".env-prod"
+ENV_ALL_FILE_NAME = ".env-prod-all"
 ENV_COMMAND_FILE_NAME = ".env-prod-cmd"
 
 def run_s(command)
@@ -15,8 +16,18 @@ def run(command, silent = false)
     command_output
 end
 
-def read_env_variables_list
-    all_env_variables_raw = run_s "heroku config --app twu-api --shell | grep -v TWU_API"
+def read_all_env_variables_list
+    read_env_variables_list true
+end
+
+def read_env_variables_list(read_all=false)
+    ignore_api_vars_str = "| grep -v TWU_API"
+
+    if read_all 
+        ignore_api_vars_str = ""
+    end
+
+    all_env_variables_raw = run_s "heroku config --app twu-api --shell #{ignore_api_vars_str}"
     all_env_variables_raw.split
 end
 
@@ -43,20 +54,14 @@ def generate_task_schedule(enclose_value_in_quotes=false)
     env_var("TASK_SCHEDULE", "0,15,30,45 * * * *", enclose_value_in_quotes)
 end
 
-task :e => :env
-task :env => [:cleanup, :env_file, :env_cmd] do
-end
-
-task :env_file do
-    all_env_variables_list = read_env_variables_list
-
-    env_variables = all_env_variables_list
+def generate_environment_file(output_filename, env_variables_list)
+    env_variables = env_variables_list
         .map { |kvstr| kvstr.split("=") }
         .map { |kvp| [kvp[0], kvp[1].chomp("'").reverse.chomp("'").reverse] }
         .map { |kvp| "#{kvp[0]}=#{kvp[1]}" }
     # puts env_variables
 
-    File.open(ENV_FILE_NAME, 'w') do |f|
+    File.open(output_filename, 'w') do |f|
         f.puts generate_task_schedule
         f.puts generate_copy_vars(env_variables)
 
@@ -65,7 +70,21 @@ task :env_file do
         }
     end
 
-    # puts `cat #{ENV_FILE_NAME}`
+    # puts `cat #{output_filename}`
+end
+
+task :e => :env
+task :env => [:cleanup, :env_file, :env_cmd, :env_all] do
+end
+
+task :env_file do
+    all_env_variables_list = read_env_variables_list
+    generate_environment_file(ENV_FILE_NAME, all_env_variables_list)
+end
+
+task :env_all do
+    all_env_variables_list = read_all_env_variables_list
+    generate_environment_file(ENV_ALL_FILE_NAME, all_env_variables_list)
 end
 
 task :env_cmd do
