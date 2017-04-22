6bedbd8e15a24408fef16ce1248eaa1b2421318e
Camilo Sanchez <camilin87@gmail.com>
2017-04-22 14:57:13 -0400

    assume a default connection string in the cron jobs if none is provided

--- cronjobs/lib/factories/dbConnection.js
+++ cronjobs/lib/factories/dbConnection.js
@@ -2,12 +2,23 @@ var promise = require("the-promise-factory");
 var MongoClient = require("mongodb").MongoClient;
 
 module.exports = function(envSettingName){
+    var DEFAULT_CONNECTION_STRING_NAME = "CONNECTION_STRING";
+
     if (!envSettingName){
-        envSettingName = "CONNECTION_STRING";
+        envSettingName = DEFAULT_CONNECTION_STRING_NAME;
     }
 
     return promise.create((fulfill, reject) => {
         var dbConnection = process.env[envSettingName];
+
+        if ((dbConnection || "").length == 0){
+            dbConnection = process.env[DEFAULT_CONNECTION_STRING_NAME];
+        }
+
+        if ((dbConnection || "").length == 0){
+            console.error("Empty connection string");
+        }
+
         MongoClient.connect(dbConnection, function(err, db){
             if (err) return reject(err);
 
